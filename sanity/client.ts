42605db1152202f85f56c73fbb5940cbcb9f1f10
Daniel Green <dangreen07@outlook.com>
2024-12-06 22:03:36 +0000

    Set dataset to production, hard coded

--- sanity/client.ts
+++ sanity/client.ts
@@ -2,7 +2,7 @@ import { createClient } from "next-sanity";
 
 export const client = createClient({
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+    dataset: "production",
     useCdn: false,
     apiVersion: "2022-03-25",
 })
